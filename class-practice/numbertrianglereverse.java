4f002b596adfddd2dc5d1bc04a3f1100b340f09b
anushkasharma26032007 <anushkasharma26032007@gmail.com>
2026-02-02 21:01:21 -1200

    Added:my programs

---
+++ class-practice/numbertrianglereverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    class numbertrianglereverse{
+         public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);		
+          int n = sc.nextInt(); 
+         for(int i=1;i<=n;i++){	 
+         for(int j=1;j<=n;j++){	
+             if(i+j<=n)		 
+				 System.out.print(" ");
+			 else System.out.print(j);
+				 
+	    	 }
+          System.out.println();
+	}
+	}
+		 }
