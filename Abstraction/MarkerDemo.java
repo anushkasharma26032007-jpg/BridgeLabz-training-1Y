a62e7be3387597bcdc62c576aad81f28f20e27c7
anushkasharma26032007 <anushkasharma26032007@gmail.com>
2026-02-20 20:28:48 -1200

    Added: My Abstraction Assignment

---
+++ Abstraction/MarkerDemo.java
@@ -0,0 +1,13 @@
+package com.gla.Abstraction;
+interface SensitiveData {}
+
+class User implements SensitiveData {
+    String password = "secret";
+}
+
+public class MarkerDemo {
+    public static void main(String[] args) {
+        User u = new User();
+        System.out.println(u instanceof SensitiveData);
+    }
+}
