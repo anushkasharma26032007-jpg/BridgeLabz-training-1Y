7c0e73191e77c93fddaaa879165fb8b58bf79049
anushkasharma26032007 <anushkasharma26032007@gmail.com>
2026-04-11 09:10:48 +0530

    Added: my classroom code program

---
+++ classroomcode/ReverseStringUsingStack.java
@@ -0,0 +1,21 @@
+package classroomcode;
+import java.util.*;
+public class ReverseStringUsingStack {
+
+    public static void main(String[] args){
+        String str = "HELLO";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<str.length();i++) {
+            stack.push(str.charAt(i));
+        }
+
+        String rev = "";
+        while (!stack.isEmpty()) {
+            rev = rev + stack.pop();
+        }
+
+        System.out.println("Reversed: " + rev);
+    }
+
+}
